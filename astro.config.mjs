1f79980f8a80442e51082b2b6f7c9dd8adadc06d
Clayton Ingalls <github@zapbampow.com>
2024-09-18 08:58:20 -0400

    adds pagefind search package to the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
+import pagefind from "astro-pagefind";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), tailwind()],
+  build: {
+    format: "file",
+  },
+  integrations: [icon(), tailwind(), pagefind()],
   experimental: {
     rewriting: true,
   },
