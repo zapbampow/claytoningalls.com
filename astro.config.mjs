fc6eb9fb99563d82e7bbe35d03de39c9b9ff3321
Clayton Ingalls <github@zapbampow.com>
2025-08-19 14:55:26 -0400

    build: adds mdx and react support

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,16 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import mdx from "@astrojs/mdx";
+
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   build: {
     format: "directory",
   },
-  integrations: [icon(), tailwind(), pagefind()],
+  integrations: [icon(), tailwind(), pagefind(), mdx(), react()],
   experimental: {
     // rewriting: true,
   },
